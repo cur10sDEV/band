3c1e181d0bf0c8e737b0221466f13bb6b058e835
specsx-codebot <specsx@protonmail.com>
2021-03-19 15:22:43 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,83 @@
+let playButton = document.querySelectorAll("button");
+
+
+// Variable Sounds
+let tom1 = new Audio("sounds/tom-1.mp3");
+let tom2 = new Audio("sounds/tom-2.mp3");
+let tom3 = new Audio("sounds/tom-3.mp3");
+let tom4 = new Audio("sounds/tom-4.mp3");
+let snare = new Audio("sounds/snare.mp3");
+let crash = new Audio("sounds/crash.mp3");
+let kick = new Audio("sounds/kick-bass.mp3");
+
+
+// Function Sounds 
+
+function switches(input) {
+
+
+	switch(input) {
+
+			case "w":
+			tom1.play();
+			break;
+
+			case "a":
+			tom2.play();
+			break;
+
+			case "s":
+			tom3.play();
+			break;
+
+			case "d":
+			tom4.play();
+			break;
+
+			case "j":
+			snare.play();
+			break;
+
+			case "k":
+			crash.play();
+			break;
+
+			case "l":
+			kick.play();
+			break;
+
+			default:
+			console.log(input);
+		}
+
+}
+
+
+
+
+// Touch Sounds
+
+for (i = 0; i < playButton.length; i++) {
+	playButton[i].addEventListener("click", function () {
+		
+		// Touch Sounds
+
+		let innerWord  = this.innerHTML;
+
+		switches(innerWord);
+
+	});
+}
+
+
+
+// Keyboard Sounds 
+
+document.addEventListener("keypress", function(e) {
+	let key = e.key;
+
+	switches(key);
+
+});
+
+
